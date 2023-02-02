62354af93af98b888deadf142ceddf7e7b47e07d
Jessica <jessikareix20@gmail.com>
2023-02-02 11:13:45 +0100

    proyecto iniciado

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const persona_a_saludar = process.argv[2] || 'amig@';
-console.log(`Hola ${persona_a_saludar}`);
-
-//console.log(`Hola amig@ ${persona_a_saludar}`);
+const persona_a_saludar = process.argv[2];
+if (!persona_a_saludar) {
+  console.error('Falta el nombre de la persona a saludar');
+  process.exit(1);
+}
+console.log(`Hola amig@ ${persona_a_saludar}`);
