6b09387135e1dde12f4c5e85ab3dc50a05ad7d34
Jessica <jessikareix20@gmail.com>
2023-02-07 00:07:54 +0100

    parámetros y variables

--- jenkinsScripts/script-3.js
+++ jenkinsScripts/script-3.js
@@ -1,13 +1,17 @@
+//varibles de entorno de Jenkins para el script
 const script1 = process.env.SCRIPT1_RESULT;
 const script2 = process.env.SCRIPT2_RESULT;
+
 
 if (script1 === 'correcto' && script2 === 'correcto') {
   console.log('El proyecto va viento en popa!!!');
   process.exit(0);
 } else if (script1 === 'incorrecto' && script2 === 'incorrecto') {
   console.error('esto pinta muy mal...');
+  console.error(script1, script2);
   process.exit(1);
 } else {
   console.log('Algún script ha fallado');
+  console.log(script1, script2);
   process.exit(1);
 }
