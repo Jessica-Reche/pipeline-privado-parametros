dca504dd4c219618634f624157454495571424e5
Jessica <jessikareix20@gmail.com>
2023-02-06 22:11:39 +0100

    parámetros y variables

--- jenkinsScripts/script-3.js
+++ jenkinsScripts/script-3.js
@@ -1,9 +1,9 @@
 const script1 = process.env.SCRIPT1_RESULT;
 const script2 = process.env.SCRIPT2_RESULT;
 
-if (script1 === '0' && script2 === '0') {
+if (script1 === 'correcto' && script2 === 'correcto') {
   console.log('El proyecto va viento en popa!!!');
-} else if (script1 !== '0' || script2 !== '0') {
+} else if (script1 !== 'correcto' || script2 !== 'correcto') {
   console.log('Alguna de las dos stages ha fallado');
 } else {
   console.log('Esto pinta muy mal');
