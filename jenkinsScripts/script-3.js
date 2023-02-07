329a2acab0aa06c58f82e8b42c5f931362eab5a4
Jessica <jessikareix20@gmail.com>
2023-02-07 12:34:01 +0100

    parámetros y variables

--- jenkinsScripts/script-3.js
+++ jenkinsScripts/script-3.js
@@ -3,13 +3,30 @@ var script1 = process.args[2];
 var script2 = process.args[3];
 
 
-if (script1 === 'correcto' && script2 === 'correcto') {
-  console.log('El proyecto va viento en popa!!!');
-  process.exit(0);
-} else if (script1 === 'incorrecto' && script2 === 'incorrecto') {
-  console.error('esto pinta muy mal...');
-  process.exit(1);
-} else {
-  console.error('Algún script ha fallado');
-  process.exit(1);
+switch (script1) {
+  case 'correcto':
+    switch (script2) {
+      case 'correcto':
+        console.log('El proyecto va viento en popa!!!');
+        process.exit(0);
+        break;
+      default:
+        console.error('Algún script ha fallado');
+        process.exit(1);
+    }
+    break;
+  case 'incorrecto':
+    switch (script2) {
+      case 'incorrecto':
+        console.error('esto pinta muy mal...');
+        process.exit(1);
+        break;
+      default:
+        console.error('Algún script ha fallado');
+        process.exit(1);
+    }
+    break;
+  default:
+    console.error('Algún script ha fallado');
+    process.exit(1);
 }
